fd75372e0d713f2abe5a628a9649905bd8ecfc0a
wolfgunblood <jalajdorai1@gmail.com>
2024-07-28 21:07:23 +0530

    update with no error files

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,5 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
