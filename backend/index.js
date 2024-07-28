1c0983d758a5affeb20cd3fce05efbae8a6a5b84
wolfgunblood <jalajdorai1@gmail.com>
2024-07-28 20:06:34 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const router = require("./routes/app");
 const authRouter = require("./routes/auth");
 const notFound = require("./middleware/not-found");
 const errorHandleMiddleware = require("./middleware/error-handler");
 const authenticateUser = require("./middleware/authentication");
 const connectDB = require("./db/connectDB");
+
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authenticateUser, router);
