1fb1ca3b624f4e3545a338921456f07dbde43b44
wolfgunblood <jalajdorai1@gmail.com>
2024-07-29 11:26:40 +0530

    added timestamps

--- backend/models/Tasks.js
+++ backend/models/Tasks.js
@@ -1,36 +1,39 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "must provide title"],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  status: {
-    type: String,
-    enum: {
-      values: ["todo", "in-progress", "under-review", "finished"],
-      message: "{VALUE} is not supported",
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "must provide title"],
+      trim: true,
     },
-    required: [true, "Please provide status"],
-  },
-  priority: {
-    type: String,
-    enum: {
-      values: ["low", "medium", "urgent"],
-      message: "{VALUE} is not supported",
+    description: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["todo", "in-progress", "under-review", "finished"],
+        message: "{VALUE} is not supported",
+      },
+      required: [true, "Please provide status"],
+    },
+    priority: {
+      type: String,
+      enum: {
+        values: ["low", "medium", "urgent"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    deadline: { type: Date },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
     },
   },
-  deadline: { type: Date },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Please provide user"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Tasks", TaskSchema);
