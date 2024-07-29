45feadbbff95c59f64817fb473fe0cc8c9ff1023
wolfgunblood <jalajdorai1@gmail.com>
2024-07-29 23:32:19 +0530

    checking cors

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
 app.use(cors());
+
+app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authenticateUser, router);
